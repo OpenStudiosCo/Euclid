e85d329553578c3033bdd6aa0c30fa653b74bd02
Paul Brzeski <mail@paulbrzeski.com>
2026-06-23 01:11:09 +0000

    Removing unused UI from first MVP

--- js/ui/Tools.js
+++ js/ui/Tools.js
@@ -4,23 +4,18 @@ export default class Tools {
     const tools = $(
         `<div id="euclid-container" class="imgedit-group imgedit-euclid">
           <div class="imgedit-group-controls">
-            <h2>Preview Layout</h2>
-            <p>
-                <button disabled class="button">Side by side</button>
-                <button disabled class="button">Top and bottom</button>
-                <button disabled class="button">Overlay</button>
-            </p>
             <h2>Output Options</h2>
+            <p>
+                Method:
+                <select id="euclid-vectorisation-method" onchange="window.euclid.changeMethod()">
+                    <option value="potrace">Black and White</option>
+                    <option value="imagetracer" selected>Colour</option>
+                </select>
+            </p>
             <p>
                 <button onclick="window.euclid.save()" class="button">Save SVG</button>
                 <button disabled class="button">Download SVG</button>
             </p>
-            <h2>Vectorisation Settings</h2>
-            Method:
-            <select id="euclid-vectorisation-method" onchange="window.euclid.changeMethod()">
-                <option value="potrace">Potrace</option>
-                <option value="imagetracer" selected>ImageTracer</option>
-            </select>
           </div>
         </div>`,
     );
